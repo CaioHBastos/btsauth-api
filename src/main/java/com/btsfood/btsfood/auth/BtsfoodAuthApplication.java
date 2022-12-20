c90b5ddb5b7f7a923e0978c0e03aa4ed301e1860
CaioHBastos <caio.cbastos@hotmail.com>
2022-12-19 21:26:28 -0300

    Adicionando arquivos do btsauth

---
+++ src/main/java/com/btsfood/btsfood/auth/BtsfoodAuthApplication.java
@@ -0,0 +1,13 @@
+package com.btsfood.btsfood.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtsfoodAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtsfoodAuthApplication.class, args);
+	}
+
+}
