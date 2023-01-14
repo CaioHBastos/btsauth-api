285d6135027220f1242c333d995da5da68aa713f
CaioHBastos <caio.cbastos@hotmail.com>
2023-01-14 13:35:16 -0300

    Implementação do authorization

--- src/main/java/com/btsfood/btsfood/auth/WebSecurityConfig.java
+++ src/main/java/com/btsfood/btsfood/auth/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -23,6 +24,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .withUser("joao")
                 .password(passwordEncoder().encode("123456"))
                 .roles("ADMIN");
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        return super.userDetailsService();
     }
 
     @Bean
